077af97253aec8456645df33b7e7705d78b2e9aa
Aminthas Neto <thas.neto@gmail.com>
2025-08-14 11:17:43 -0300

    refactoring functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,39 +46,39 @@ const n1Field = document.getElementById('nota1');
 const n2Field = document.getElementById('nota2');
 const n3Field = document.getElementById('nota3');
 const result = document.getElementById('result');
-const botao = document.getElementById('botao');
+const botaoCalc = document.getElementById('botao');
 
 //Funcao Botao Salvar
-botao.addEventListener ("click", (event) => {
-	const nome = nameField.value;
+botaoCalc.addEventListener ("click", () => {
+	const nome = nameField.value.trim();
 	const n1 = parseFloat(n1Field.value) || 0;
 	const n2 = parseFloat(n2Field.value) || 0;
 	const n3 = parseFloat(n3Field.value) || 0;
 
-	result.innerHTML = calcularResultado(nome, n1, n2, n3);
-
-	if (typeof(nome) != 'string') {
+	if (typeof(nome) == '') {
 		alert('Nome invalido!');
 		return;
 	}
 
-	if (n1 < 0 || n1 > 100 ) {
+	if (n1 < 0 || n1 > 100) {
 		alert('Nota 1 invalida! Digite valores entre 0 e 100.');
 		return
 	}
 
-	if (n2 < 0 || n2 > 100 ) {
+	if (n2 < 0 || n2 > 100) {
 		alert('Nota 2 invalida! Digite valores entre 0 e 100.');
 		return
 	}
 
-	if (n3 < 0 || n3 > 100 ) {
+	if (n3 < 0 || n3 > 100) {
 		alert('Nota 3 invalida! Digite valores entre 0 e 100.');
 		return
 	}
+
+	result.innerHTML = calcMedia(nome, n1, n2, n3);
 });
 
-function calcularResultado (nome, n1, n2, n3) {	
+const calcMedia = (nome, n1, n2, n3) => {	
 	const media = (n1 + n2 + n3) / 3;
 
 	if (media < 50) {
